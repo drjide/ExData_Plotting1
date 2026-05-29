1d1cce21383f5185f62e3f70bf606e36daca3460
Olujide Arije <115163798+drjide@users.noreply.github.com>
2026-05-29 17:31:31 +0200

    Add data download and preprocessing for plot2
    Download and read household power consumption data, filtering for specific dates and creating a datetime column.

--- plot2.R
+++ plot2.R
@@ -1,3 +1,22 @@
+library(tidyverse)
+library(lubridate)
+
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipfile <- "power_data.zip"
+txtfile <- "household_power_consumption.txt"
+
+if(!file.exists(txtfile)){
+  download.file(url, destfile = zipfile)
+  unzip(zipfile)
+}
+
+hhpower <- read_delim("household_power_consumption.txt", 
+                      delim = ";", na = "?") |> 
+  filter(Date %in% c("1/2/2007", "2/2/2007")) |> 
+  mutate(
+    datetime = lubridate::dmy_hms(paste(Date, Time))
+  )
+
 png("plot2.png", width = 480, height = 480)
 
 
